65bd16ed63fa71930a9487ae3b2d557f8a0fde13
Elisabeth Røysland <elisabeth.skartveit@lyse.net>
2025-09-03 10:53:25 +0200

    A persons original wage is set as the input value on the hireform, but you can still change the hire offer

--- src/pages/PersonProfile/components/HireForm.jsx
+++ src/pages/PersonProfile/components/HireForm.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
 import PropTypes from 'prop-types'
 
@@ -11,6 +11,11 @@ function HireForm({person, onHire}) {
     onHire({...person, wage})
     navigate("/")
   }
+
+  useEffect(() => {
+    setWage(person.wage || 0)
+  }, [person])
+
 
   return (
     <form onSubmit={handleSubmit}>
